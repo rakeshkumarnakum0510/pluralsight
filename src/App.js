25ac2eb150a93de9ddceadff201b4b8c06df7c07
rakeshkumarnakum0510 <rakeshkumarnakum0510@gmail.com>
2019-04-05 17:59:59 +0530

    initial chnages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Form from './container/form';
+import Card from './component/card';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+const CardList = (props) => (
+	<div>
+  	{props.profiles.map(profile => <Card {...profile}/>)}
+	</div>
+);
+
+class App extends React.Component {
+  state = {
+    profiles: [],
+  };
+  addNewProfile = (profileData) => {
+   this.setState(prevState => ({
+     profiles: [...prevState.profiles, profileData],
+   }));
+    
+  };
+	render() {
+  	return (
+    	<div>
+    	  <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile}/>
+        <CardList profiles={this.state.profiles} />
+    	</div>
     );
-  }
+  }	
 }
+
+
 
 export default App;
